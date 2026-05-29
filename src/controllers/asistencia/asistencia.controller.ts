d883e28979c23b83a6472976ed655dd5b1c9c44a
KleberMera <klebermera2016@gmail.com>
2026-05-29 17:13:17 -0500

    feat: ajustar formato de fecha en registros de asistencia a la zona horaria de Guayaquil

--- src/controllers/asistencia/asistencia.controller.ts
+++ src/controllers/asistencia/asistencia.controller.ts
@@ -100,7 +100,7 @@ export const registrarAsistencia = async (req: Request, res: Response) => {
         a.cedula,
         a.id_brigada,
         b.nombre_brigada,
-        a.created_at
+        CONVERT_TZ(a.created_at, @@session.time_zone, 'America/Guayaquil') as created_at
       FROM asistencia a
       LEFT JOIN brigadas b ON a.id_brigada = b.id_brigada
       WHERE a.id = ?`,
@@ -136,7 +136,7 @@ export const obtenerAsistencias = async (req: Request, res: Response) => {
       a.cedula,
       a.id_brigada,
       b.nombre_brigada,
-      a.created_at
+      CONVERT_TZ(a.created_at, @@session.time_zone, 'America/Guayaquil') as created_at
     FROM asistencia a
     LEFT JOIN brigadas b ON a.id_brigada = b.id_brigada`;
 
@@ -205,7 +205,7 @@ export const obtenerAsistenciasPorBrigadaActiva = async (
         a.cedula,
         a.id_brigada,
         b.nombre_brigada,
-        a.created_at
+        CONVERT_TZ(a.created_at, @@session.time_zone, 'America/Guayaquil') as created_at
       FROM asistencia a
       LEFT JOIN brigadas b ON a.id_brigada = b.id_brigada
       WHERE a.id_brigada = ?
